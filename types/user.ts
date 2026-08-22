3a8253996cb873589ade4ed63ee341a915a5c120
jessiecatubay <catubayfrancis7@gmail.com>
2026-08-23 00:17:32 +0800

    new changes

--- types/user.ts
+++ types/user.ts
@@ -6,3 +6,11 @@ export interface AuthUser {
   firstName?: string;
   lastName?: string;
 }
+
+export interface Vital {
+  id: string;
+  deviceId: string;
+  temperature: number;
+  heartRate: number;
+  recordedAt: string;
+}
